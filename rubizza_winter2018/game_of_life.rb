6af65c90ad5061cc061419a68560bd13253b9fb2
Pavel Zhachko <rammiel@yandex.ru>
2018-11-29 14:38:09 +0300

    game of life w/o exceptions, maybe some logig is broken and code looks like a poo

--- rubizza_winter2018/game_of_life.rb
+++ rubizza_winter2018/game_of_life.rb
@@ -38,7 +38,7 @@ end
 # class Universe
 class Universe
   def initialize
-    @universe = Array.new(3) { Array.new(3) { Cell.new } }
+    @universe = Array.new(6) { Array.new(6) { Cell.new } }
   end
 
   def array_border_check
@@ -46,63 +46,48 @@ class Universe
 
   def check(cell_x, cell_y)
     count = 0
-    #p "[#{cell_x}][#{cell_y}]"
-    #p self.show
     cell_x = -1 if cell_x >= @universe.size || cell_x + 1 >= @universe.size
     cell_y = -1 if cell_y >= @universe[0].size || cell_y + 1 >= @universe[0].size
-    #p "passed #{cell_x} and #{cell_y}"
     @universe[cell_x - 1][cell_y - 1].alive? ? count += 1 : count
-    #p "[#{cell_x}][#{cell_y}]"
-    #p "[cell_x - 1][cell_y - 1]"
     @universe[cell_x][cell_y - 1].alive? ? count += 1 : count
-    #p "[#{cell_x}][#{cell_y}]"
-    #p "[cell_x][cell_y - 1]"
     @universe[cell_x + 1][cell_y - 1].alive? ? count += 1 : count
-    #p "[#{cell_x}][#{cell_y}]"
-    #p "[cell_x + 1][cell_y - 1]"
     @universe[cell_x - 1][cell_y].alive? ? count += 1 : count
-    #p "[#{cell_x}][#{cell_y}]"
-    #p "[cell_x - 1][cell_y]"
     @universe[cell_x + 1][cell_y].alive? ? count += 1 : count
-    #p "[#{cell_x}][#{cell_y}]"
-    #p "#{[cell_x + 1]}#{[cell_y]}"
-    #p "wtf"
-    #p "#{[cell_x - 1]}#{[cell_y + 1]}"
     @universe[cell_x - 1][cell_y + 1].alive? ? count += 1 : count
-    #p "f*ck"
-    #p "[#{cell_x}][#{cell_y}]"
-    #p "#{[cell_x - 1]}#{[cell_y + 1]}"
     @universe[cell_x][cell_y + 1].alive? ? count += 1 : count
-    #p "[#{cell_x}][#{cell_y}]"
-    #p "[cell_x][cell_y + 1]"
     @universe[cell_x + 1][cell_y + 1].alive? ? count += 1 : count
-    #p "[#{cell_x}][#{cell_y}]"
-    #p "[cell_x + 1][cell_y + 1]"
-    case
-    when count == 2, 3 && @universe[cell_x][cell_y].alive?
-      @universe[cell_x][cell_y].live
-    when count < 2 || count > 3
-      @universe[cell_x][cell_y].dead
-    else
-      puts 'Error'
+    if @universe[cell_x][cell_y].alive?
+      case
+      when count == 2 || count == 3
+        @universe[cell_x][cell_y].live
+      when count < 2 || count > 3
+        @universe[cell_x][cell_y].dead
+      else
+        puts 'Error'
+      end
+    elsif @universe[cell_x][cell_y].dead?
+      @universe[cell_x][cell_y].live if count == 3
     end
-  end
-
-  def universe
-    @universe[0][0]
   end
 
   def neighborhood
     @universe.each do |row|
       row.each do |col|
-        #p "row: #{@universe.index(row)} - col: #{row.index(col)}"
         check(@universe.index(row),row.index(col))
-        self.show
       end
     end
+    self.show
   end
 
   def show
-    @universe.map { |row| row.map { |el| el.show_status } }
+    @universe.map { |row| p row.map { |el| el.show_status } }
   end
+end
+
+u = Universe.new
+#p u.show
+while true
+  sleep 1
+  system("clear")
+  u.neighborhood
 end
